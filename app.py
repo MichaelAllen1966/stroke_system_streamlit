a0857dca7ef101ac5e52984d3c209eecc5825cb3
Michael Allen <michael.allen1966@gmail.com>
2025-06-10 11:21:19 +0100

    Add columns

--- app.py
+++ app.py
@@ -23,25 +23,7 @@ params = {
 
 st.title('Simple stroke system model')
 
-# Add a description
-
-# Add an image
-st.image('./images/system_1.png', caption='Flow diagram of the stroke system', width=350)
-
-st.write("""
-This is a model that simulates the flow of patients through a simple stroke system:
-- HASU: Hyper Acute Stroke Unit
-- ASU: Acute Stroke Unit
-- ESD: Early Supported Discharge
-         
-
-Patients are admitted to the HASU and then may be transferred to the ASU. The model simulates the flow of patients through HASU, ASU and ESD.
-- The length of stay (LOS) for each component is defined by a mean (a log-normal distribution with 70% CV is applied to add typical variation in LOS).
-- The model parameters can be adjusted using the sidebar.
-- The model is run by clicking the "Run model" button (clicking again will simulate another year).
-""")
-
-# Add a box to enter admissions_per_year
+# Add a box to enter parameters
 st.sidebar.title("Model parameters")
 params['admissions_per_year'] = st.sidebar.number_input('Admissions per year', value=params['admissions_per_year'], step=50)
 params['prop_hasu_using_asu'] = st.sidebar.number_input('Proportion HASU patients using ASU', value=params['prop_hasu_using_asu'], step=0.05)
@@ -52,22 +34,41 @@ params['los_asu_no_esd_mean'] = st.sidebar.number_input('Mean LOS (days) in ASU 
 params['los_asu_with_esd_mean'] = st.sidebar.number_input('Mean LOS (days) in ASU with ESD', value=params['los_asu_with_esd_mean'])
 params['los_esd_mean'] = st.sidebar.number_input('Mean LOS (days) in ESD', value=params['los_esd_mean'])
 
+# Add a description
+
+col_1, col_2 = st.columns(2)
+
+with col_1:
+
+    # Add an image
+    st.image('./images/system_1.png', caption='Flow diagram of the stroke system', width=350)
+
+    st.write("""
+    This is a model that simulates the flow of patients through a simple stroke system:
+    - HASU: Hyper Acute Stroke Unit
+    - ASU: Acute Stroke Unit
+    - ESD: Early Supported Discharge
+            
+
+    Patients are admitted to the HASU and then may be transferred to the ASU. The model simulates the flow of patients through HASU, ASU and ESD.
+    - The length of stay (LOS) for each component is defined by a mean (a log-normal distribution with 70% CV is applied to add typical variation in LOS).
+    - The model parameters can be adjusted using the sidebar.
+    - The model is run by clicking the "Run model" button (clicking again will simulate another year).
+    """)
 
 
-if st.button('Run model'):
-    # Load the model with the parameters
-    m = Model(params)
+with col_2:
 
-    warm_up = 100
-    sim_duration = 365 # Duration after warm up
-    m.run(warm_up, sim_duration)
 
-    # Create 2 columns for the outputs
-    col1, col2 = st.columns(2)
+    if st.button('Run model'):
+        # Load the model with the parameters
+        m = Model(params)
 
-    with col1:
+        warm_up = 100
+        sim_duration = 365 # Duration after warm up
+        m.run(warm_up, sim_duration)
+
         st.image('./output/bed_occupancy.png', caption='Bed Occupancy', use_container_width=True)
-    with col2:
         st.write(m.system.audit_report_summary)
 
 
